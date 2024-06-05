59b4690018b196172abe3c0e4a49a0adf659f920
Ashton <ashtonhowa@gmail.com>
2024-06-05 19:21:57 -0400

    fixing preview image in spots again

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -32,6 +32,7 @@ for(let el of spots){
         createdAt:el.createdAt,
         updatedAt:el.updatedAt
     }
+
     let image = await Image.findOne({where:{
         preview:true,
         imageableId:el.id
@@ -45,28 +46,6 @@ for(let el of spots){
 }
 
 
- // include:{
-    //     model:Image,
-    //     where:{preview:true},
-    //     as:'previewImage',
-    //     attributes:['url'],
-
-    // }
-
-
-
-// spots.forEach((el) => {
-//     console.log(el);
-//     // console.log(el.previewImage[0].url)
-//     let url = el.previewImage[0].url
-//     el.previewImage= url;
-// })
-// let images = await Image.findAll({
-//     where: {
-//         preview:true,
-//         imageableType:'Spot'
-//     }
-// })
 res.json(arr);
 
 });
