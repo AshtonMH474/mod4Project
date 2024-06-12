a7c7b359e4d1df2b850e9496d994bf880379ab2a
Ashton <ashtonhowa@gmail.com>
2024-06-12 18:47:33 -0400

    deleting image for review and spot complete

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -4,7 +4,11 @@ const sessionRouter = require('./session.js');
 const usersRouter = require('./users.js');
 const spotsRouter = require('./spots.js')
 const reviewRouter = require('./reviews.js');
+const { Op} = require('sequelize');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const { restoreUser } = require("../../utils/auth.js");
+const {Image,User,Review,Spot} = require('../../db/models');
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
@@ -21,4 +25,67 @@ router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
 
+
+router.delete('/review-images/:imageId', async(req,res) => {
+  const {token} = req.cookies;
+    const decodedPayload = jwt.decode(token);
+
+    let userId = Number(decodedPayload.data.id);
+    let imageId = req.params.imageId;
+
+
+
+    let foundImage = await Image.findOne({
+        where:{
+            id:Number(imageId),
+            imageableType: 'Review'
+        }
+    })
+
+    let review = await Review.findOne({where: {
+        id:foundImage.imageableId,
+        userId:userId
+    }});
+
+    if(token && foundImage && review){
+      await foundImage.destroy();
+      res.json({
+        message: "Successfully deleted"
+      })
+    }else res.status(404).json({
+      message: "Review Image couldn't be found"
+    })
+})
+
+
+router.delete('/spot-images/:imageId', async(req,res) => {
+  const {token} = req.cookies;
+    const decodedPayload = jwt.decode(token);
+
+    let userId = Number(decodedPayload.data.id);
+    let imageId = req.params.imageId;
+
+
+
+    let foundImage = await Image.findOne({
+        where:{
+            id:Number(imageId),
+            imageableType: 'Spot'
+        }
+    })
+
+    let spot = await Spot.findOne({where: {
+        id:foundImage.imageableId,
+        ownerId:userId
+    }});
+
+    if(token && foundImage && spot){
+      await foundImage.destroy();
+      res.json({
+        message: "Successfully deleted"
+      })
+    }else res.status(404).json({
+      message: "Spot Image couldn't be found"
+    })
+})
 module.exports = router;
