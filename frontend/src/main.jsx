32590169b315820acf25e0906b3f81efc5f0935c
Ashton <ashtonhowa@gmail.com>
2024-07-17 21:31:39 -0400

    phase 1 done

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,21 +4,23 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import configureStore from './store';
-// import { restoreCSRF,csrfFetch } from './store/csrf';
+import { restoreCSRF,csrfFetch } from './store/csrf';
+import * as sessionActions from './store/session'
 const store = configureStore();
 
 
 
-// if (import.meta.env.MODE !== 'production') {
-//   restoreCSRF();
+if (import.meta.env.MODE !== "production") {
+  restoreCSRF();
 
-//   window.csrfFetch = csrfFetch;
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+  window.sessionActions = sessionActions; // <-- ADD THIS LINE
+}
+
+// if (process.env.NODE_ENV !== 'production') {
 //   window.store = store;
 // }
-
-if (process.env.NODE_ENV !== 'production') {
-  window.store = store;
-}
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
