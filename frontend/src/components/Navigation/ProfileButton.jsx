369879a40e7cb41e042e1b9b485a74565c6e3926
Ashton <ashtonhowa@gmail.com>
2024-07-18 22:30:50 -0400

    authenticate me is done

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,80 +1,87 @@
 import { CgProfile } from "react-icons/cg";
+
 import { useState, useEffect, useRef } from 'react';
 import { useDispatch } from 'react-redux';
 import * as sessionActions from '../../store/session';
+import OpenModalButton from '../OpenModalButton';
+import LoginFormModal from '../LoginFormModal';
+import SignupFormModal from '../SignupFormModal';
 
 function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
+  const ulRef = useRef();
 
-        const dispatch = useDispatch();
-        const [showMenu, setShowMenu] = useState(false);
-        const ulRef = useRef();
+  const toggleMenu = (e) => {
+    e.stopPropagation(); // Keep from bubbling up to document and triggering closeMenu
+    setShowMenu(!showMenu);
+  };
 
-        const toggleMenu = (e) => {
-          e.stopPropagation(); // Keep click from bubbling up to document and triggering closeMenu
-          // if (!showMenu) setShowMenu(true);
-          setShowMenu(!showMenu);
-        };
+  useEffect(() => {
+    if (!showMenu) return;
 
-        useEffect(() => {
-          if (!showMenu) return;
+    const closeMenu = (e) => {
+      if (!ulRef.current.contains(e.target)) {
+        setShowMenu(false);
+      }
+    };
 
-          const closeMenu = (e) => {
-            if (ulRef.current && !ulRef.current.contains(e.target)) {
-              setShowMenu(false);
-            }
-          };
+    document.addEventListener('click', closeMenu);
 
-          document.addEventListener('click', closeMenu);
+    return () => document.removeEventListener("click", closeMenu);
+  }, [showMenu]);
 
-          return () => document.removeEventListener('click', closeMenu);
-        }, [showMenu]);
+  const logout = (e) => {
+    e.preventDefault();
+    dispatch(sessionActions.logout());
+    setShowMenu(false)
+  };
 
-        const logout = (e) => {
-          e.preventDefault();
-          dispatch(sessionActions.logout());
-        };
-
-        const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
-
-
-      if(showMenu == true){
-        return (
-            <>
-              <button onClick={toggleMenu}>
-                <CgProfile />
-              </button>
-              <ul className={ulClassName}>
-                <li>{user.username}</li>
-                <li>{user.firstName} {user.lastName}</li>
-                <li>{user.email}</li>
-                <li>
-                  <button onClick={logout}>Log Out</button>
-                </li>
-              </ul>
-            </>
-          );
-      }else{
-
+  const ulClassName = "profile-dropdown" + (showMenu ? "" : " hidden");
+  if(showMenu == true){
+  return (
+    <>
+      <button onClick={toggleMenu}>
+        <CgProfile />
+      </button>
+      <ul className={ulClassName} ref={ulRef}>
+        {user ? (
+          <>
+            <li>{user.username}</li>
+            <li>{user.firstName} {user.lastName}</li>
+            <li>{user.email}</li>
+            <li>
+              <button onClick={logout}>Log Out</button>
+            </li>
+          </>
+        ) : (
+          <>
+            <li>
+              <OpenModalButton
+                buttonText="Log In"
+                modalComponent={<LoginFormModal />}
+              />
+            </li>
+            <li>
+              <OpenModalButton
+                buttonText="Sign Up"
+                modalComponent={<SignupFormModal />}
+              />
+            </li>
+          </>
+        )}
+      </ul>
+    </>
+  );
+}else{
     return (
-      <>
-        <button onClick={toggleMenu}>
-          <CgProfile />
+        <>
+         <button onClick={toggleMenu}>
+             <CgProfile />
         </button>
-        {/* <ul className={ulClassName}>
-          <li>{user.username}</li>
-          <li>{user.firstName} {user.lastName}</li>
-          <li>{user.email}</li>
-          <li>
-            <button onClick={logout}>Log Out</button>
-          </li>
-        </ul> */}
-      </>
-    );
+        </>
+    )
 }
-  }
+}
 
-
-
-
-
-  export default ProfileButton;
+export default ProfileButton;
