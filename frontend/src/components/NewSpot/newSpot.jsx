2eb252ad644501e6ffcf41a81efde41a1fd07a12
Ashton <ashtonhowa@gmail.com>
2024-07-24 23:14:18 -0400

    same message as last one

--- frontend/src/components/NewSpot/newSpot.jsx
+++ frontend/src/components/NewSpot/newSpot.jsx
@@ -17,13 +17,16 @@ function SpotForm(){
     const [image3, setImage3] = useState('Image URL');
     const [image4, setImage4] = useState('Image URL');
 
+
+
+
     return (
         <>
         <form onSubmit='' className='newSpotForm'>
         <h1>Create a New Spot</h1>
-        <div id='locationInfo'>
+    <div id='borderNew'>
         <h2>Where&apos;s your place located?</h2>
-        <div>Guests will only get your exact address once they booked a
+        <div className='aboveL'>Guests will only get your exact address once they booked a
         reservation.</div>
 
         <label>Country</label>
@@ -31,49 +34,68 @@ function SpotForm(){
          onChange={(e) => setCountry(e.target.value)}
          required/>
 
+        <div className='address'>
         <label>Street Address</label>
         <input type='text' value={address}
          onChange={(e) => setAddress(e.target.value)}
          required/>
-
-        <label>City</label>
-        <input type='text' value={city}
-         onChange={(e) => setCity(e.target.value)}
-         required/>
-
-        <label>State</label>
-        <input type='text' value={state}
-         onChange={(e) => setState(e.target.value)}
-         required/>
-
-        <label>Latitude</label>
-        <input type='text' value={lat}
-         onChange={(e) => setLat(e.target.value)}
-         required/>
-
-
-        <label>Longitude</label>
-        <input type='text' value={log}
-         onChange={(e) => setLog(e.target.value)}
-         required/>
          </div>
+        <div className='locationSpot'>
+
+            <div className='city'>
+            <label>City</label>
+            <input type='text' value={city}
+            onChange={(e) => setCity(e.target.value)}
+            required/>
+            ,
+            </div>
+
+            <div className='state'>
+            <label>State</label>
+            <input type='text' value={state}
+            onChange={(e) => setState(e.target.value)}
+            required/>
+            </div>
+        </div>
+
+        <div className='locationLL'>
+            <div className='lat'>
+            <label>Latitude</label>
+            <input type='text' value={lat}
+            onChange={(e) => setLat(e.target.value)}
+            required/>
+
+            </div>
+            ,
+            <div className='long'>
+            <label>Longitude</label>
+            <input type='text' value={log}
+             onChange={(e) => setLog(e.target.value)}
+             required/>
+             </div>
+        </div>
+    </div>
 
 
-        <div>
+        <div  className='description'>
             <h2>Describe your place to guests</h2>
-            <div>Mention the best features of your space, any special amentities like
+            <div className='aboveL'>Mention the best features of your space, any special amentities like
             fast wif or parking, and what you love about the neighborhood.</div>
 
 
-            <input type='text' value={description}
+            <textarea type='text' value={description}
             onChange={(e) => setDescription(e.target.value)}
-            required/>
+            required>
+            {description}
+            </textarea>
+
+
 
         </div>
 
-        <div>
+        <div id='borderNew'>
             <h2>Create a title for your spot</h2>
-            <div>Catch guest&apos;s attention with a spot title that highlights what makes
+            <div className='aboveL'>Catch guest&apos;s attention with a spot title that highlights what makes
             your place special.</div>
             <input type='text' value={name}
             onChange={(e) => setName(e.target.value)}
@@ -81,9 +103,9 @@ function SpotForm(){
 
         </div>
 
-        <div>
+        <div id='borderNew' >
             <h2>Set a base price for your spot</h2>
-            <div>Competitive pricing can help your listing stand out and rank higher
+            <div className='aboveL'>Competitive pricing can help your listing stand out and rank higher
             in search results</div>
             $ <input type='text' value={money}
             onChange={(e) => setMoney(e.target.value)}
@@ -91,9 +113,9 @@ function SpotForm(){
         </div>
 
 
-        <div>
+        <div id='borderNew'>
             <h2>Liven up your spot with photos</h2>
-            <div>Submit a link to at least one photo to publish your spot.</div>
+            <div className='aboveL'>Submit a link to at least one photo to publish your spot.</div>
             <input type='text' value={previewImage}
             onChange={(e) => setPreview(e.target.value)}
             required/>
