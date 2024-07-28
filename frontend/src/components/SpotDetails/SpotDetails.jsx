c3a77f01710f2bdf8a9c435efc07f4421f68305c
Ashton <ashtonhowa@gmail.com>
2024-07-27 20:40:40 -0400

    same as last message

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -1,35 +1,36 @@
-// import { useDispatch,useSelector } from "react-redux";
+import { useDispatch,useSelector } from "react-redux";
 // import { useEffect, useState } from "react";
-// import { CiStar } from "react-icons/ci";
-// import { useParams } from "react-router-dom";
-// import { detailsOfSpot } from "../../store/spots";
+import { useEffect } from "react";
+import { CiStar } from "react-icons/ci";
+import { useParams } from "react-router-dom";
+import { detailsOfSpot } from "../../store/spots";
 import './SpotDetails.css'
 // import { reviewsForSpot } from "../../store/reviews";
 // import OpenModalButton from "../OpenModalButton";
 // import CreateReview from "../CreateReview";
 
  function SpotDetails (){
-    // const {spotId} = useParams();
-    // const dispatch = useDispatch();
-    // const spot = useSelector((state) => state.spots);
+    const {spotId} = useParams();
+    const dispatch = useDispatch();
+    const spot = useSelector((state) => state.spots);
     // const reviews = useSelector((state) => state.reviews)
     // const sessionUser = useSelector(state => state.session.user);
     // const [reviewList, setReviewList] = useState([]);
 
 
 
-    //   useEffect(() => {
-    //     const getData = async () => {
-    //         dispatch(detailsOfSpot(spotId));
-    //        const reviews = await dispatch(reviewsForSpot(spotId))
-    //     //    console.log(reviews)
-    //     //    console.log(spot)
-    //        setReviewList(Object.values(reviews));
-    //     }
+      useEffect(() => {
+        const getData = async () => {
+            dispatch(detailsOfSpot(spotId));
+        //    const reviews = await dispatch(reviewsForSpot(spotId))
+        //    console.log(reviews)
+        //    console.log(spot)
+        //    setReviewList(Object.values(reviews));
+        }
 
-    //     getData();
+        getData();
 
-    //   }, [dispatch,spotId]);
+      }, [dispatch,spotId]);
 
     //   const handleModalClose = async () => {
     //     // Refresh the reviews and spot details
@@ -45,10 +46,10 @@ import './SpotDetails.css'
     //     return date.toLocaleDateString('en-US', { month: 'long', year: 'numeric' });
     //   };
 
-    // if(!spot.SpotImages) return <div>Loading...</div>;
+    if(!spot.SpotImages) return <div>Loading...</div>;
 
-    // let preview = spot.SpotImages.find((image) => image.preview == true);
-    // let imageArray = spot.SpotImages.filter((image) => image.preview == false);
+    let preview = spot.SpotImages.find((image) => image.preview == true);
+    let imageArray = spot.SpotImages.filter((image) => image.preview == false);
     // let presentReview;
     // if(sessionUser){
     // presentReview = reviewList.find((review) => sessionUser.id ==  review.userId)
@@ -58,68 +59,68 @@ import './SpotDetails.css'
 
 
 return (
+
     <>
-    <h2>Hello</h2>
+
+    <div className="spotContainer">
+        <div className="spotInfo">
+    <h2 className="spotH2">{spot.name}</h2>
+    <p className="spotLocation">{spot.city}, {spot.state}, {spot.country}</p>
+        </div>
+    <div className="images">
+        <div className="spotPreview">
+        <img className="previewDetails" src={preview.url} alt='preview'/>
+        </div>
+
+        {imageArray.map((image,i) => (
+        <div  key={image.id} className={`spotImage${i}`}>
+        <img className="currImage" key={image.id} src={image.url} alt='image' />
+        </div>
+        ))}
+
+    </div>
+
+        <div className="ownerD">
+        <h2 className="spotName">Hosted by {spot.Owner.firstName} {spot.Owner.lastName}</h2>
+        <p className="spotD">
+        {spot.description}
+        </p>
+        </div>
+
+        <div className="spotRating">
+
+            <div id='avgReview'>
+
+             {spot.avgStarRating > 0 && ( <div className="avg" ><CiStar className="star"/>{spot.avgStarRating}</div>)}
+             {spot.avgStarRating <= 0 && ( <div className="avgNew"><CiStar className="starNew"/>New</div>)}
+
+             {spot.numReviews > 1 && (<div className="spotReview">
+                {spot.numReviews} reviews
+            </div>)}
+            {spot.numReviews == 1 && (<div className="spotReview">
+                {spot.numReviews} review
+            </div>)}
+
+            </div>
+
+            <div className="spotPricing">
+            <div className='pricing'>
+                <div id="spotPrice">${spot.price}</div>
+                <div id='night'>night</div>
+            </div>
+            </div>
+
+
+            <div className="buttonR">
+            <button className="reserve">
+                Reserve
+            </button>
+            </div>
+        </div>
+
+    </div>
     </>
-//     <>
-
-//     <div className="spotContainer">
-//         <div className="spotInfo">
-//     <h2 className="spotH2">{spot.name}</h2>
-//     <p className="spotLocation">{spot.city}, {spot.state}, {spot.country}</p>
-//         </div>
-//     <div className="images">
-//         <div className="spotPreview">
-//         <img className="previewDetails" src={preview.url} alt='preview'/>
-//         </div>
-
-//         {imageArray.map((image,i) => (
-//         <div  key={image.id} className={`spotImage${i}`}>
-//         <img className="currImage" key={image.id} src={image.url} alt='image' />
-//         </div>
-//         ))}
-
-//     </div>
-
-//         <div className="ownerD">
-//         <h2 className="spotName">Hosted by {spot.Owner.firstName} {spot.Owner.lastName}</h2>
-//         <p className="spotD">
-//         {spot.description}
-//         </p>
-//         </div>
-
-//         <div className="spotRating">
-
-//             <div id='avgReview'>
-
-//              {spot.avgStarRating > 0 && ( <div className="avg" ><CiStar className="star"/>{spot.avgStarRating}</div>)}
-//              {spot.avgStarRating <= 0 && ( <div className="avgNew"><CiStar className="starNew"/>New</div>)}
-
-//              {spot.numReviews > 1 && (<div className="spotReview">
-//                 {spot.numReviews} reviews
-//             </div>)}
-//             {spot.numReviews == 1 && (<div className="spotReview">
-//                 {spot.numReviews} review
-//             </div>)}
-
-//             </div>
-
-//             <div className="spotPricing">
-//             <div className='pricing'>
-//                 <div id="spotPrice">${spot.price}</div>
-//                 <div id='night'>night</div>
-//             </div>
-//             </div>
-
-
-//             <div className="buttonR">
-//             <button className="reserve">
-//                 Reserve
-//             </button>
-//             </div>
-//         </div>
-
-//     </div>
+)
 
 
 //     <div className="reviewsPerSpot">
@@ -135,7 +136,7 @@ return (
 //             </h2>)}
 //         </div>
 
-//         {sessionUser && !presentReview && spot.Owner.id != sessionUser.id && (
+        //  {/* {sessionUser && !presentReview && spot.Owner.id != sessionUser.id && ( */}
 //              <OpenModalButton  className='CreateReview'
 //              buttonText="Create your Review"
 //              modalComponent={<CreateReview spotId={spotId} refresh={handleModalClose} />}
@@ -167,8 +168,8 @@ return (
 
 
 //     </div>
-//     </>
-)
+    // </>
+// )
 }
 
 export default SpotDetails
