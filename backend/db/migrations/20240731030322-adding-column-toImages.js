3066ad181e639c98954b7226475d2be84e03d4af
Ashton <ashtonhowa@gmail.com>
2024-08-01 12:19:38 -0400

    fixing migartion fro adding public id column

--- backend/db/migrations/20240731030322-adding-column-toImages.js
+++ backend/db/migrations/20240731030322-adding-column-toImages.js
@@ -1,12 +1,15 @@
 'use strict';
-
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.addColumn('Images', 'publicId', {
       type: Sequelize.STRING, // Specify the data type and other attributes
          // Adjust constraints as needed
-    });
+    },options);
 
     /**
      * Add altering commands here.
@@ -17,7 +20,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Images', 'publicId');
+    await queryInterface.removeColumn('Images', 'publicId',options);
     /**
      * Add reverting commands here.
      *
